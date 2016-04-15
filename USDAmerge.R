16c603d5d13a99a04d00966ea30252c8d22c35b0
Erich Seamon <erichs@uidaho.edu>
2016-04-14 21:32:05 -0700

    altered USDAMerge

--- USDAmerge.R
+++ USDAmerge.R
@@ -1,12 +1,15 @@
+#-------------
+#    author: Erich Seamon
+#    script: USDAmerge.R
+#    description:  merges many csv files
+#
+#-----------
+
 setwd("/git/data/USDA/zip")
 path <- setwd("/git/data/USDA/zip")
-
-
 myfiles <- list.files(path = path, pattern = ".csv",  full.names = FALSE)
-
-USDAarchive <- do.call("rbind", lapply(myfiles, read.csv, header = FALSE))
-
 multmerge = function(mypath){
   filenames=list.files(path=mypath, full.names=TRUE)
   datalist = lapply(filenames, function(x){read.csv(file=x,header=T)})
   Reduce(function(x,y) {merge(x,y)}, datalist)
+}
